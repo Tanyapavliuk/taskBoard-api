1bcca0d1687a0da329ec4cd4391ebd9b376d4fe2
Tetiana Pavliuk <122897433+Tanyapavliuk@users.noreply.github.com>
2024-03-16 19:07:34 +0200

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 
@@ -11,6 +12,8 @@ const HOST = process.env.HOST || 'http://localhost';
 const PORT = parseInt(process.env.PORT || '4500');
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
